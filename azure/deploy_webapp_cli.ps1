000dd1286100144fb4f6d805b789a246ba1655a7
Morten la Cour <lacour@gmail.com>
2024-04-27 08:31:37 +0200

    added deploy to webapp

---
+++ azure/deploy_webapp_cli.ps1
@@ -0,0 +1,43 @@
+param (
+    [string]$rgName,
+    [string]$webAppName,
+    [string]$subscriptionId = '',
+    [string]$slot = 'production'
+)
+
+if ($subscriptionId.Length -eq 0)
+{
+    $subscriptionId = az account show | ConvertFrom-Json | Select-Object -ExpandProperty id;
+    Write-Host("Setting SubscriptionId to current CLI Id: ${subscriptionId}");
+}
+
+Write-Host("Removing existing published files");
+
+if (Test-Path .\publish\) {
+    Get-Item .\publish\ | Remove-Item -Recurse -Force
+}
+
+if (Test-Path .\publish.zip) {
+    Get-Item .\publish.zip | Remove-Item -Recurse -Force
+}
+
+
+Write-Host("Build/Publish .NET code");
+
+dotnet publish -c Release -o ./publish
+
+Write-Host("Zip..");
+
+cd .\publish
+Compress-Archive -Force -Path * -DestinationPath ..\publish.zip
+cd ..
+
+Write-Host("Publish to Azure: ${rgName}/${webAppName}. Slot: ${slot}");
+
+az webapp deploy `
+        --resource-group $rgName `
+        --name $webAppName `
+        --src-path ./publish.zip `
+        --subscription $subscriptionId `
+        --slot $slot
+;
